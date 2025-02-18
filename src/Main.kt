94b13e5c5ad0fcc52951420d9067376b6467e1fc
athoob123 <athoob.alroumi@gmail.com>
2025-02-18 17:53:52 +0300

    null safety task 1

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,22 @@
 fun main() {
-    println("Hello World!")
+    var age: Int? = 25
+    age = null
+    println("Age: $age")
+
+    var city: String? = "Kuwait"
+    city = null
+
+    val length = city?.length ?: 0
+    println("City Length: $length")
+
+    val validScore = 85.0
+    val nullScore: Double? = null
+
+    println("New score is valid input: ${addPoints(validScore)}")
+    println("New score is null input: ${addPoints(nullScore)}")
+
+}
+
+fun addPoints(score: Double?): Double {
+    return (score ?: 0.0) + 5.0
 }
